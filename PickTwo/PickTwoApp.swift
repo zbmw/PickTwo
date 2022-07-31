117a0c829c1777e01de6648b1c075ed676c47c65
zbmw <waltonbmw@gmail.com>
2022-07-31 14:48:52 -0500

    Initial Commit

---
+++ PickTwo/PickTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickTwoApp.swift
+//  PickTwo
+//
+//  Created by Brett Walton on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PickTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
