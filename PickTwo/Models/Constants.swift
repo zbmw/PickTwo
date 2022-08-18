de169228f9900bcc434dbb9dd277cd9a0e7633e9
zbmw <waltonbmw@gmail.com>
2022-08-17 19:51:56 -0500

    Picks page, 1.0 Build 2

---
+++ PickTwo/Models/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  PickTwo
+//
+//  Created by Brett Walton on 8/17/22.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessToken = "BEARER + YOUR ACCESS TOKEN"
+    static let accessTokenRoot = "YOUR ACCESS TOKEN"
+}
--- PickTwo/Network.swift
+++ PickTwo/Network.swift
@@ -7,20 +7,23 @@
 
 import Foundation
 import SwiftUI
+import Firebase
 
 class Network: ObservableObject {
     @Published var polls: Poll?
     @Published var teams: [Team] = []
+    @Published var user: UserProfile? 
+    
     var rankedTeams: [Team] {
         let teams = self.teams.filter({$0.rank != nil}).sorted(by: {$0.rank ?? 99 < $1.rank ?? 98})
         return teams
     }
 
     func getRankings() {
-        guard let url = URL(string: "https://api.collegefootballdata.com/rankings?year=2021&week=1&seasonType=postseason") else { fatalError("Missing URL") }
+        guard let url = URL(string: "https://api.collegefootballdata.com/rankings?year=2022&week=1&seasonType=regular") else { fatalError("Missing URL") }
 
         var urlRequest = URLRequest(url: url)
-        urlRequest.setValue("YOUR API KEY HERE", forHTTPHeaderField: "Authorization")
+        urlRequest.setValue(Constants.accessToken, forHTTPHeaderField: "Authorization")
 
         let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             if let error = error {
@@ -50,11 +53,70 @@ class Network: ObservableObject {
         dataTask.resume()
     }
     
+    func getUser(id: String) -> UserProfile? {
+        let db = Firestore.firestore()
+        var ref: DocumentReference
+        if !id.isEmpty {
+            ref = db.collection("users").document(id)
+        } else {
+            return nil
+        }
+        
+        ref.getDocument { document, error in
+            guard error == nil else {
+                print("Error: \(error?.localizedDescription ?? "")")
+                return
+            }
+            if let document = document, document.exists {
+                let data = document.data()
+                
+                let uid = data?["uid"] as? String ?? ""
+                let currentPicks = data?["currentPicks"] as? [String] ?? []
+                let name = data?["name"] as? String ?? ""
+                let previousPicks = data?["previousPicks"] as? [String] ?? []
+                if uid == id {
+                    if self.user == nil {
+                        self.user = UserProfile()
+                    }
+                    self.user?.id = uid
+                    self.user?.currentPicks = currentPicks
+                    self.user?.name = name
+                    self.user?.previousPicks = previousPicks
+                    print("User was found")
+                }
+            } else {
+                print("snapshot does not exist")
+            }
+        }
+        if user == nil {
+            print("User was not found")
+        }
+        return user
+    }
+    
+    func setPicks(picks: [Team], id: String, name: String, previousPicks: [String]) {
+        var friendlyNames: [String]? = []
+        for pick in picks {
+            friendlyNames?.append(pick.school)
+        }
+        let db = Firestore.firestore()
+        let ref = db.collection("users").document(id)
+        ref.setData(["currentPicks": friendlyNames ?? [nil,nil],
+                     "uid": id,
+                     "name": name,
+                    "previousPicks": previousPicks]) { error in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            }
+        }
+        friendlyNames = nil
+    }
+    
     func getTeams() {
         guard let url = URL(string: "https://api.collegefootballdata.com/teams/fbs?year=2022") else { fatalError("Missing URL") }
 
         var urlRequest = URLRequest(url: url)
-        urlRequest.setValue("YOUR API KEY HERE", forHTTPHeaderField: "Authorization")
+        urlRequest.setValue(Constants.accessToken, forHTTPHeaderField: "Authorization")
 
         let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             if let error = error {
@@ -93,9 +155,6 @@ class Network: ObservableObject {
             print("Assigned \(team.school) a ranking of \(team.rank)")
         }
     }
-    
-    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-    }
+
     
 }
